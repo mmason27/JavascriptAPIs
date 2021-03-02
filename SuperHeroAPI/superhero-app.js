db6aeaa9172874d380e1404e31875e462d9154a9
mmason27 <mmmason27@gmail.com>
2021-03-02 08:40:30 -0500

    changed content that displays to user

--- SuperHeroAPI/superhero-app.js
+++ SuperHeroAPI/superhero-app.js
@@ -1,11 +1,11 @@
 //first we save our variables from the HTML document so we can later reference and append our content to it
 let moviesContainer = document.getElementById("moviesContainer");
-let userTextBox = document.getElementById("userTextBox");
+let movieDetails = document.getElementById("movieDetails");
 let submitButton = document.getElementById("submitButton");
-let movieURL = "http://www.omdbapi.com/?s=batman&apikey=26993a44";
 
 //now let's create a function that will display movies on the screen
 function displayMovies() {
+    let movieURL = "http://www.omdbapi.com/?s=batman&apikey=26993a44";
 
     //create our first request to the api to get the initial movie details
     let request = new XMLHttpRequest();
@@ -14,14 +14,15 @@ function displayMovies() {
 
     //add an event handler so that when the data we want loads, we can append it to our html page so users can view it
     request.addEventListener('load', function() {
-        //JSON.parse 'parses' a string, constructing it into a JS value or object, making it easier to retrieve the information. Here we save it in a variable to make it easier to iterate through the data
+        // JSON.parse 'parses' a string, constructing it into a JS value or object, making it easier to retrieve the information. Here we save it in a variable to make it easier to iterate through the data
         let movies = JSON.parse(request.responseText);
+        // next we'll map through the items since we now have an array, by iterating through the array we can return a template literal with the information we want from the original array
         let movieItems = movies.Search.map((movie) => {
-            return ` 
-                <img src="${movie.Poster}" id="moviePoster">
+            return `<div id="movieThumbnail">
                 <h2 id="movieTitle">${movie.Title}</h2>
-                <p id="movieYear">${movie.Year}</p>
-                <button onClick=displayDetails('${movie.imdbID}')>Click here for more details</button>`
+                <img src="${movie.Poster}" id="moviePoster">
+                <p id="movieYear">Released in ${movie.Year}</p>
+                <button onClick=displayDetails('${movie.imdbID}')>Click here for more details</button></div>`
                 
     })
     //now we need to append this content to our div in index.html
@@ -29,7 +30,6 @@ function displayMovies() {
     moviesContainer.innerHTML = movieItems.join("");
 })
 }
-
 //create a function so that we can click a button on the page 'more info' and it will display more info from the api using the imdbID found in the initial list of all batman movies
 displayMovies();
 
@@ -41,8 +41,15 @@ function displayDetails(imdbID) {
 
     request.addEventListener('load', function () {
         let detail = JSON.parse(request.responseText);
-        console.log(detail)
-
-    let movieContent = 
+        let detailItem = 
+                    `
+                    <div id="addtlInfo">
+                    <img src="${detail.Poster}" id="bigMoviePoster">
+                    <p id="rated">Rating: ${detail.Rated}</p>
+                    <p id="releaseDate">Release Date: ${detail.Released}</p>
+                    <p id="director">Directed By: ${detail.Director}</p>
+                    </div>
+                    `
+    movieDetails.innerHTML = detailItem;
     })
 }
