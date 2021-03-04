febec326330e504508e3a4675741cc75c58dc1ea
Megan Mason <mmmason27@gmail.com>
2021-03-04 08:52:45 -0500

    refactored code to create separate functions

--- WeatherApp/weatherapp.js
+++ WeatherApp/weatherapp.js
@@ -1,29 +1,37 @@
 const cityTextBox = document.getElementById("cityTextBox");
-const enterCityButton = document.getElementById("enterCityButton");
+const enterCityStateButton = document.getElementById("enterCityStateButton");
 const infoDisplay = document.getElementById("infoDisplay");
+const stateTextBox = document.getElementById("stateTextBox");
 
-enterCityButton.addEventListener('click', () => {
+//getting the city and state from the user
+enterCityStateButton.addEventListener('click', () => {
     const city = cityTextBox.value;
-    infoDisplay.innerHTML = "";
+    const state = stateTextBox.value;
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=19609be724a8df10c209346015e71fe9`)
-    .then((response) => {
-        return response.json()
-    }) .then((posts) => {
-        const post = posts.main;
-        console.log(post)
-        const weatherItem = `<h1 id="cityName">Forecast for ${city}</h1>
-                    <div id="weatherDetails">
-                    <p id="minTemp">Today's Low: ${kelvinToFarenh(post.temp_min)}<span>&#176;</span> F</p>
-                    <p id="maxTemp">Today's High: ${kelvinToFarenh(post.temp_max)}<span>&#176;</span> F</p>
-                    <p id="pressure">Pressure: ${post.pressure}</p></div>`
-        infoDisplay.innerHTML = weatherItem;
-        }) 
+    fetchWeather(city, state)
+    cityTextBox.value = "";
+    stateTextBox.value = "";
 })
 
-function kelvinToFarenh(kelvin) {
-    let farenheit = ((kelvin-273.15)*1.8)+32;
-    return Math.round(farenheit);
+//function for fetching the weather from the api
+function fetchWeather(city, state) {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${state}&appid=19609be724a8df10c209346015e71fe9&units=imperial`)
+    .then((response) => { 
+        return response.json();
+    }) .then((posts) => {
+        console.log(posts.main);
+        displayWeather(city, state, posts);
+    })
 }
 
-
+//function for displaying the weather to the user
+function displayWeather(city, state, posts) {
+    let post = posts.main;
+    const weatherItem = `<h1 id="cityName">Forecast for ${city}, ${state}</h1>
+                    <div id="weatherDetails">
+                    <p id="weather">${posts.weather[0].main}</p>
+                    <p id="minTemp">Today's Low: ${Math.round(post.temp_min)}<span>&#176;</span> F</p>
+                    <p id="maxTemp">Today's High: ${Math.round(post.temp_max)}<span>&#176;</span> F</p>
+                    <p id="pressure">Pressure: ${post.pressure}</p></div>`
+    infoDisplay.innerHTML = weatherItem; 
+}
